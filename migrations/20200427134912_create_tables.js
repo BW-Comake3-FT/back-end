326aa57a67044e87a1518e08e031bc4dddb7ae54
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-27 13:56:09 -0400

    Create 'users' and 'projects' tables, knex migration/setup

---
+++ migrations/20200427134912_create_tables.js
@@ -0,0 +1,28 @@
+exports.up = function(knex) {
+    return knex.schema
+
+    // Users table
+    .createTable("users", tbl => {
+        tbl.increments();
+        tbl.string("name", 128).notNullable();
+        tbl.string("email", 128).notNullable().unique();
+        tbl.string("password", 128).notNullable();
+        tbl.integer("zip", 128).notNullable();
+    })
+
+    // Projects table
+    .createTable("projects", tbl => {
+        tbl.increments();
+        tbl.string('title', 255).notNullable();
+        tbl.string('description', 140).notNullable();
+        tbl.string('location', 140).notNullable();
+        tbl.timestamp('timestamp').defaultTo(knex.fn.now());
+        tbl.integer("user_id").references("id").inTable("users").onUpdate("CASCADE").onDelete("CASCADE").notNullable();
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+    .dropTableIfExists("projects")
+    .dropTableIfExists("users");
+};
