0ba6c7caea14e4deb81fcc3363b362e474fb8879
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-28 18:50:21 -0400

    Add token to process.env, complete tests for projects endpoint

--- data/projectRouter.test.js
+++ data/projectRouter.test.js
@@ -1,19 +1,46 @@
 const request = require("supertest");
 const server = require("../app");
 
+const token = process.env.TOKEN;
+
+const project = {
+    "title": "Improve infrastructure",
+    "description": "We'll need to raise funding.",
+    "location": "Chicago",
+    "category": "Government",
+    "solution": "ETC"
+};
+
 describe("projectRouter test suite", () => {
     // Tokens expire after 24 hr
-    describe("Return projects", () => {
+    describe("Get all projects endpoint", () => {
         it("Should return 200 status with valid token", async () => {
             const res = await request(server).get("/api/projects")
-            .set("Authenticate", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6Ildvd29vdyIsImVtYWlsIjoiYmxhaEB5YWhvby5jb20iLCJwYXNzd29yZCI6IiQyYSQxMCR3dEtqQjBsdmVuS0IzVkRmVFpYWTlPbWt1REk3LnNlZi5QeGduYmtzUWNFVFFiMWhvazFVdSIsInppcGNvZGUiOiIxMDAwMSIsImlhdCI6MTU4ODAyNjg0NiwiZXhwIjoxNTg4MTEzMjQ2fQ.Pht6zooneYKhS8HjmL0rk0plFSbulkLe2g6-GTva7RI");
+            .set("Authenticate", token);
             expect(res.statusCode).toEqual(200);
-        })
+        });
 
         it("Should return 401 status with invalid token", async () => {
             const res = await request(server).get("/api/projects")
-            .set("Authenticate", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6Ildvd29vdyIsImVtYWlsIjoiYmxhaEB5YWhvby5jb20iLCJwYXNzd29yZCI6IiQyYSQxMCR3dEtqQjBsdmVuS0IzVkRmVFpYWTlPbWt1REk3LnNlZi5QeGduYmtzUWNFVFFiMWhvazFVdSIsInppcGNvZGUiOiIxMDAwMSIsImlhdCI6MTU4ODAyNjg0NiwiZXhwIjoxNTg4MTEzMjQ2fQ.Pht6zooneYKhS8HjmL0rk0plFSbulkLe2g6-GTva7R");
+            .set("Authenticate", "invalid-token");
             expect(res.statusCode).toEqual(401);
+        });
+    })
+
+    describe("Get project by ID endpoint", () => {
+        it("Should return 200 status with valid token", async () => {
+            // Get first project
+            const res = await request(server).get("/api/projects/1")
+            .set("Authenticate", token);
+            expect(res.statusCode).toEqual(200);
         })
     })
-});
+
+    describe("Add new project", () => {
+        it("Should return 500 status", async () => {
+            const res = await request(server).post("/api/projects").send(project)
+            .set("Authenticate", token);
+            expect(res.statusCode).toBe(500);
+        })
+    })
+})
