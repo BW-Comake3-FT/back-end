12578b7ace9a67ae1f74314938bc9f8011c78bf4
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-28 16:32:05 -0400

    Another CORS fix

--- app.js
+++ app.js
@@ -15,6 +15,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 server.use(cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use('/api/auth', authRouter);
 app.use('/api/projects', authMiddle.validateToken, projectRouter);
 
